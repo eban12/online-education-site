f70413cfbfcc4920bc9dccc746ccbd12587ad653
Eyob <eban@pop-os.localdomain>
2021-05-31 23:24:59 +0300

    git init

---
+++ backend/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config.from_mapping (
+    SECRET_KEY = 'secretkey',
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace('postgres://', 'postgresql://'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+
+db = SQLAlchemy(app)
+
+from .api import api
+app.register_blueprint(api)
