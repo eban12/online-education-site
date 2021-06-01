b3a668fa9f0e9ca518256841a4adde48433769b7
Eyob <eban@pop-os.localdomain>
2021-06-02 00:20:38 +0300

    authentication functionality added with /auth endpoint

---
+++ backend/app/api/auth_route.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request
+from flask_restx import Resource, Api
+from werkzeug.security import check_password_hash
+from app.models import User
+import jwt
+import datetime
+from app import app
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+auth_api = Api(auth_bp)
+
+@auth_api.route("/")
+class Auth(Resource):
+    def get(self):
+        auth = request.authorization
+
+        if not auth or not auth.username or not auth.password:
+            return make_response("Could not verify", 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
+        user = User.query.filter_by(email=auth.username).first()
+
+        if not user:
+            return make_response("Could not verify", 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+        
+        if check_password_hash(user.password, auth.password):
+            token = jwt.encode({
+                        'public_id': user.public_id,
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)     
+                    }, app.config['SECRET_KEY'], algorithm="HS256")
+            return {"token": token}
+        
+        return make_response("Could not verify", 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
